3d0a24953360a068b6a91dcdd8643dd522bea93e
nassira5832 <nassira.aitmehdi@gmail.com>
2024-11-17 23:43:26 +0100

    2nd

--- Knight.py
+++ Knight.py
@@ -16,12 +16,58 @@ class  Chromosome :
             if (random.random()<mutation_rate):
                 self.genes[i] = random.randint(0, 7)
 class  Knight :
-    def __init__(self , position , chromosome , path , fitness=0):
-        self.position=position
-        self.position=position 
-        self.chromosome = chromosome 
-        self.path= path 
-        self.fitness= fitness
+    def __init__(self, chromosome=None):
+        self.position = (0, 0) 
+        self.chromosome = chromosome if chromosome else Chromosome()  
+        self.path = [self.position]  
+        self.fitness = 0  
+    def move_forward (self ,direction):
+        x, y = self.position
+        if (direction==1):
+             dx, dy=(-2, +1)
+        if (direction==2):
+             dx, dy=(-1, +2)
+        if (direction==3):
+             dx, dy=(+1, +2)
+        if (direction==4):
+             dx, dy=(+2, +1)
+        if (direction==5):
+             dx, dy=(+2, -1)
+        if (direction==6):
+             dx, dy=(+1, -2)
+        if (direction==7):
+             dx, dy=(-1, -2)
+        if (direction==8):
+             dx, dy=(-2, -1)
+        new_position = (x + dx, y + dy)
+        self.position = new_position
+        self.path.append(new_position)
+        return new_position
     
+    def move_backward (self,direction):
+         
+         if (direction==1):
+             dx, dy=(-2, +1)
+         if (direction==2):
+             dx, dy=(-1, +2)
+         if (direction==3):
+             dx, dy=(+1, +2)
+         if (direction==4):
+             dx, dy=(+2, +1)
+         if (direction==5):
+             dx, dy=(+2, -1)
+         if (direction==6):
+             dx, dy=(+1, -2)
+         if (direction==7):
+             dx, dy=(-1, -2)
+         if (direction==8):
+             dx, dy=(-2, -1)
+         self.position = (self.position[0] - dx, self.position[1] - dy)
+         self.path.pop()
+    def check_moves ( ):
+        
+
+
+
         
     
